aaa47db8e88de6b602aae99d026d90f1647db8aa
James Brown <github@mesbrown.co.uk>
2016-01-17 22:40:03 +0000

    Title component takes title from props.children

--- source/components/title/index.js
+++ source/components/title/index.js
@@ -1,2 +1,2 @@
-export default React => ({ titleClass, title }) =>
-  <h1 className={ titleClass }>{ title }</h1>
+export default React => ({ titleClass, children }) =>
+  <h1 className={ titleClass }>{ children }</h1>
